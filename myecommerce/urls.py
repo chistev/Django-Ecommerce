7753e646746a5baefe0a33aca21b58fd10da923a
chistev <stephenowabie@gmail.com>
2023-12-21 20:24:53 +0100

    Added a second card and added a slideshow on it

--- myecommerce/urls.py
+++ myecommerce/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('ecommerce.urls')),
 ]
+urlpatterns += staticfiles_urlpatterns()
