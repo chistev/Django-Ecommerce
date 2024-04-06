e08476b401690e3660fe7fce28fded7ee8c76a07
chistev <stephenowabie@gmail.com>
2024-04-06 01:00:11 +0100

    modified the html for the register and personal details pages to make them responsive since i had previously altered the base template; Also, made the delete account page responsive and wrote tests for the delete account view for correct password and incorrect password

--- accounts/tests.py
+++ accounts/tests.py
@@ -9,7 +9,7 @@ from django.urls import reverse, resolve
 
 
 from accounts.models import CustomUser, Address, PersonalDetails, State, City
-from accounts.views import change_password
+from accounts.views import change_password, delete_account
 
 
 class LoginOrRegisterViewTest(TestCase):
@@ -83,4 +83,64 @@ class MyAccountViewTest(TestCase):
         self.assertEqual(response.context['user'], self.user)
         self.assertEqual(response.context['personal_details'], self.personal_details)
         self.assertQuerysetEqual(response.context['user_addresses'], Address.objects.filter(user=self.user))
+
+
+class DeleteAccountViewTest(TestCase):
+    def setUp(self):
+        self.factory = RequestFactory()
+        self.user = CustomUser.objects.create(email='test@example.com', password=make_password('testpassword'))
+
+    def test_post_delete_account_correct_password(self):
+        url = reverse('accounts:delete_account')
+        data = {
+            'confirm_delete': '1',
+            'Password': 'testpassword'  # Correct password
+        }
+
+        request = self.factory.post(url, data)
+        request.user = self.user
+
+        # Create a session to store messages
+        middleware = SessionMiddleware(lambda x: None)
+        middleware.process_request(request)
+        request.session.save()
+
+        # Attach message storage to request
+        messages = FallbackStorage(request)
+        setattr(request, '_messages', messages)
+
+        response = delete_account(request)
+        self.assertTrue(response.url.startswith(reverse('ecommerce:index')))
+
+        # Check if success message is displayed
+        messages = list(get_messages(request))
+        self.assertEqual(len(messages), 1)
+        self.assertEqual(str(messages[0]), 'Your account has been successfully deleted.')
+
+    def test_post_delete_account_incorrect_password(self):
+        url = reverse('accounts:delete_account')
+        data = {
+            'confirm_delete': '1',
+            'Password': 'incorrectpassword'  # Incorrect password
+        }
+
+        request = self.factory.post(url, data)
+        request.user = self.user
+
+        # Create a session to store messages
+        middleware = SessionMiddleware(lambda x: None)
+        middleware.process_request(request)
+        request.session.save()
+
+        # Attach message storage to request
+        messages = FallbackStorage(request)
+        setattr(request, '_messages', messages)
+
+        response = delete_account(request)
+        self.assertEqual(response.status_code, 200)
+
+        # Check if error message is displayed
+        messages = list(get_messages(request))
+        self.assertEqual(len(messages), 1)
+        self.assertEqual(str(messages[0]), 'Incorrect password. Please try again.')
 
